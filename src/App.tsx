ed7f373bcef0227e064a1e18588c5cb0b7c62d57
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-01-24 13:27:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import PWABadge from './PWABadge.tsx'
+
+function App() {
+  return (
+    <>
+   
+      <h1 className=' text-6xl text-center'>kaotika-battle-mobile</h1>
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
